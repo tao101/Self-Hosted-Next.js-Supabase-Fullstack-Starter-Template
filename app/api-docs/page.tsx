a0eecf65b9a64f8dad9ff0af871b4facd9c568a7
Taoufiq Lotfi <admin@taoufiqlotfi.tech>
2025-02-04 13:06:18 +0200

    Minor cleanup in API docs page rendering configuration

--- app/api-docs/page.tsx
+++ app/api-docs/page.tsx
@@ -2,7 +2,7 @@ import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 import { swaggerSpec } from '@/utils/api-docs';
 
-export const dynamic = 'force-dynamic'; // Add this line
+export const dynamic = 'force-dynamic';
 
 export default function APIDocs() {
   return <SwaggerUI spec={swaggerSpec} />;
